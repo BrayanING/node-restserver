d11908ed6757ef94155b3c01ed17fc58ccdff479
Josué <josue@Josues-Mac.local>
2018-09-19 17:05:56 -0700

    Add list

---
+++ server/models/listas.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let listaSchema = new Schema({
+    descripcion: {
+        type: String,
+        uinque: true,
+        required: [true, 'La lista es requreida']
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+});
+
+module.exports = mongoose.model('Categoria', categoriaSchema);
